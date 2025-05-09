df73f3d967a4bc759d7810d570d2a6e07fb41ec8
Jura Danilov <jura.danilov.2024@gmail.com>
2025-05-10 01:14:13 +0300

    Initial commit

--- bankCardService/src/test/java/com/yurdan/bankCardService/BankCardManagementSystemApplicationTests.java
+++ bankCardService/src/test/java/com/yurdan/bankCardService/BankCardManagementSystemApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.yurdan.bankCardService;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BankCardManagementSystemApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.yurdan.bankCardService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
